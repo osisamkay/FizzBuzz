257e0f6d3066015a7444b209e2e7bd961d9ddb65
osisamkay <43083088+osisamkay@users.noreply.github.com>
2018-11-03 19:32:19 +0100

    modified

--- fizzbuzz js/fizzbuzz.js
+++ fizzbuzz js/fizzbuzz.js
@@ -1,12 +1,16 @@
-for (index = 1; index <= 100; index++) {
-    if (index % 3 == 0) {
-        document.write("Fizz");
-    }
-    if (index % 5 == 0) {
-        document.write("Buzz");
-    }
-    if ((index % 3 != 0) && (index % 5 != 0)) {
-        document.write(index);
+for (let index = 1; index < 100; index++) {
+    if (((index % 3) || (index % 5)) == 0) {
+        document.write("FizzBuzz");
+    } else {
+        if ((index % 3) == 0) {
+            document.write("Fizz");
+        } else {
+            if ((index % 5) == 0) {
+                document.write("Buzz")
+            } else {
+                document.write(index);
+            }
+        }
     }
     document.write(`<br>`);
 }
